82f7c2d5dcd4056cb1ad20acb95b65d9f174ef7e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-12-02 01:23:27 -0500

    Just Download Gin and MongoDB

--- U.sh
+++ U.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m " Refactor "
+git commit -m " Just Download Gin and MongoDB "
 git push -u origin main 
---
+++ mongoDB/01-Basic/A2- Image-1/B1/main.go
@@ -0,0 +1,12 @@
+/* Download:
+	
+	go get github.com/gin-gonic/gin
+	go get go.mongodb.org/mongo-driver/mongo
+
+*/
+
+package main
+
+func main()  {
+	
+}
